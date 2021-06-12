04d54cda3ba20509c3728d0e7b33f645d88d2b43
Storm <storm.sarracino01@gmail.com>
2021-06-12 15:23:56 -0500

    commit tree 9ac3ba88c1d682fd96a233c03accaba306f4075d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    let resultblocks = 42 - blocks;
+    return Math.abs(resultblocks);
+}
+
+function distanceFromHqInFeet(blocks) {
+    let feet = distanceFromHqInBlocks(blocks);
+    return feet * 264;
+}
+
+function distanceTravelledInFeet(start, finish) {
+    let totalDistance = (finish - start) * 264;
+    return Math.abs(totalDistance);
+}
+
+function calculatesFarePrice(start, destination) {
+    let d = distanceTravelledInFeet(start, destination);
+    let farePrice;
+        if (d <= 400){
+            farePrice = 0;
+        }
+        else if (d > 400 && d < 2000){
+            farePrice = .02 * (d - 400);
+        }
+        else if (d > 2000 && d < 2500 ){
+            farePrice = 25;
+        }
+        else if (d > 2500){
+            farePrice = 'cannot travel that far';
+        }
+        return farePrice;
+}
